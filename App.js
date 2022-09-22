0c3e6d4dd9fe4664d65582ba7fe7b0012860b1b7
Adityakr3 <Adityakumar14022002@gmail.com>
2022-09-23 02:27:18 +0530

    First commit

---
+++ App.js
@@ -0,0 +1,42 @@
+const api_url = 
+"https://satsin06.github.io/customApis/swim_results.json";
+
+// Defining async function
+async function getapi(url) {
+
+// Storing response
+const response = await fetch(url);
+
+// Storing data in form of JSON
+var data = await response.json();
+console.log(data);
+if (response) {
+  hideloader();
+}
+show(data);
+}
+// Calling that async function
+getapi(api_url);
+
+// Function to hide the loader
+function hideloader() {
+    document.getElementById('loading').style.display = 'none';
+}
+// Function to define innerHTML for HTML table
+function show(data) {
+    let tab = 
+        `<tr>
+          <th>Name</th>
+          <th>Time</th>
+         </tr>`;
+    
+    // Loop to access all rows 
+    for (let r of data) {
+        tab += `<tr> 
+    <td>${r.name} </td>
+    <td>${r.time}</td>
+</tr>`;
+    }
+    // Setting innerHTML as tab variable
+    document.getElementById("swimers").innerHTML = tab;
+}
